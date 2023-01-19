222520303712fc71ff9d5a7cd9b30390a54f8954
rose534 <wambuirose534@gmail.com>
2023-01-20 01:40:41 +0300

    about component

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>Trendy Threads is a clothing and fashion retail company that specializes in offering the latest trends and styles to our customers. We believe that fashion should be accessible to everyone, and we work hard to bring you high-quality clothing at affordable prices.</p>
+      <p>Our team is made up of fashion experts and dedicated professionals who are passionate about providing our customers with the best shopping experience possible. We currently employ 50 people, all working together to bring you the latest fashions and trends from around the world.</p>
+    </div>
+  );
+}
+
+export default About;
